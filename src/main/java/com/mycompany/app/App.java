00432b6ed6e357fe58cd4969bf40786298028a90
Olasubomi Odekunle <subomi.odekunle@flutterwavego.com>
2024-05-25 19:46:25 +0100

    init

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World 4!";
+    private static final String MESSAGE = "Hello World 5!";
 
     public App() {}
 
